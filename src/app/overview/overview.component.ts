14ba3a2a364355b526e560a665789e9cdfcd1763
Pavel Salauyou <pavel.salauyou@gmail.com>
2025-05-01 00:18:33 +0200

    fix

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCard, MatCardContent, MatCardFooter } from '@angular/material/card';
 
 @Component({
-  selector: 'app-overview',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatCard,
+    MatCardContent,
+    MatCardFooter
+  ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
